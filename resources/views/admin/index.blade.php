7568809495fa0dc9cb1c5ae888b2ee09a21f620c
Agustina Incicco <agustinaincicco@MacBook-Pro-de-Agustina-2.local>
2019-07-09 21:00:55 -0300

    administrar_productos

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,42 @@
 @extends('layouts.app')
 @section('content')
-<a class="btn btn-warning" href="{{url("products/create")}}">Agregar Producto</a>
-<a class="btn btn-warning" href="{{url("products/edit")}}">Editar Producto</a>
+<section id="get-started" class="padd-section text-center">
+        <div class="container">
+          <div class="section-title text-center">
+    
+            <h2>Administrador de productos</h2>
+            <p class="separator">Cheramel Design</p>
+            <hr>
+          </div>
+        </div>
+    
+        <div class="container">
+          <div class="row justify-content-center">
+    
+            <div class="col-md-6 col-lg-4">
+              <div class="feature-block">
+    
+                <img src="images/agregar.jpg" alt="img" class="img-fluid">
+                <h4></h4>
+                <p></p>
+                <a class="btn btn-warning" href="{{url("products/create")}}">Agregar Producto</a>
+    
+              </div>
+            </div>
+    
+            <div class="col-md-6 col-lg-4">
+              <div class="feature-block">
+    
+                <img src="images/editar.jpg" alt="img" class="img-fluid">
+                <h4></h4>
+                <p></p>
+                <a class="btn btn-warning" href="{{url("products/edit")}}">Editar Producto</a>
+    
+              </div>
+            </div>
+    
+          </div>
+        </div>
+    <br>
+      </section>
 @endsection
